3dcaa8791cb81825a588e6e9d5a6d05dec0ab92e
John Doherty <doherty.j@cambridgeassessment.org.uk>
2016-12-31 16:10:15 +0000

    first commit with example and basic docs

---
+++ example/server.js
@@ -0,0 +1,36 @@
+var express = require('express'),
+    handlebars = require('express-handlebars'),
+    iframeReplacement = require('../index.js');
+
+function Server() {
+
+    // create an instance of express
+    var app = express();
+
+    // add iframe replacement to express as middleware (adds res.merge method)
+    app.use(iframeReplacement);
+
+    // add handlebars view engine (you can use any)
+    app.engine('.hbs', handlebars.create({ 
+        extname: '.hbs' 
+    }).engine);
+
+    // let express know how to locate the views/templates
+    app.set('views', './example/views');
+    app.set('view engine', 'hbs');
+
+    // create simple route to test our fake news
+    app.get('/', function(req, res){
+
+        // respond to this request with our fake-new content embedded within the BBC News home page
+        res.merge('fake-news', {
+            sourceUrl: 'http://www.bbc.co.uk/news',                             // external url to fetch
+            sourcePlaceholder: 'div[data-entityid="container-top-stories#1"]'   // css selector to inject our content into
+        });
+    });
+
+    // start the server
+    app.listen(8080);
+}
+
+module.exports = new Server();
